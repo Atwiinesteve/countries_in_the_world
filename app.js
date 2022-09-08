1c0757c6654f77535da7d996adb1d82855225434
Kiiza Atwiine Stephen <stephenkiiza123@gmail.com>
2022-09-08 16:33:41 +0300

    Added Flags and View Map Link

--- app.js
+++ app.js
@@ -17,17 +17,19 @@ function showCountry(data) {
         const country = document.createElement('div');
         country.classList.add('country');
         
-        country.innerHTML = `
+        country.innerHTML += `
 
                 <div class="country__flag">
                         <img src="${data.coatOfArms.svg}" alt="" class="court_of_arms img-fluid">
+                        <img src="${data.flags.png}" alt="" class="flag img-fluid">
                 </div>
                 <div class="country__details">
-                        <div class="country__official__name">Name: ${data.name.official}</div>
+                        <div class="country__official__name">${data.name.official}</div>
                         <div class="region">Region: ${data.region}</div>
                         <div class="capital">Capital City: ${data.capital}</div>
                         <div class="independence__status">Independent: ${data.independent}</div>
                         <div class="population">Population: ${data.population}</div>
+                        <div class="map"><a href='${data.maps.googleMaps}'>view map</a></div>
                 </div>
         
         `  
